73837ede002ee4ccab6a5b17704edd24fc2e31c3
subash-v <subash.v@xelpmoc.in>
2023-03-20 18:07:59 +0530

    delete

---
+++ 78_delete_BST.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+
+void inOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d, ", root->data);
+        inOrder(root->right);
+    }
+}
+struct Node *createNode(int data)
+{
+    struct Node *n;                                 // creating a Node pointer
+    n = (struct Node *)malloc(sizeof(struct Node)); // Allocating memory in the heap
+    n->data = data;                                 // Setting the data
+    n->left = NULL;                                 // Setting the left and right children to NULL
+    n->right = NULL;                                // Setting the left and right children to NULL
+    return n;                                       // Finally returning the created Node
+}
+
+struct Node *inOrderPredecessor(struct Node *root)
+{
+    root = root->left;
+    while (root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+struct Node *deleteNode(struct Node *root, int value)
+{
+    struct Node *iPre;
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        free(root);
+        return NULL;
+    }
+
+    // Search for the node to be deleted
+    if (value < root->data)
+    {
+        root->left = deleteNode(root->left, value);
+    }
+    else if (value > root->data)
+    {
+        root->right =deleteNode(root->right, value);
+    }
+    // Deletion Strategy
+    else
+    {
+        iPre = inOrderPredecessor(root);
+        root->data = iPre->data;
+        root->left = deleteNode(root->left, iPre->data);
+    }
+
+    return root;
+}
+
+int main()
+{
+
+    // Constructing the root Node - Using Function (Recommended)
+    struct Node *p = createNode(5);
+    struct Node *p1 = createNode(3);
+    struct Node *p2 = createNode(6);
+    struct Node *p3 = createNode(1);
+    struct Node *p4 = createNode(4);
+    // Finally The tree looks like this:
+    //      5
+    //     / \
+    //    3   6
+    //   / \
+    //  1   4
+
+    // Linking the root Node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    inOrder(p);
+    deleteNode(p, 5);
+    printf("\n");
+    inOrder(p);
+
+    return 0;
+}
